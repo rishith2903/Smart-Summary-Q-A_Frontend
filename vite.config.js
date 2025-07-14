d81a401cf73fcf6d8d04311ec77bd1fe57d680b2
rishith2903 <138714387+rishith2903@users.noreply.github.com>
2025-07-14 18:57:11 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Smart-Summary-Q-A_Frontend/', // change this if your GitHub repo name is different
+});
